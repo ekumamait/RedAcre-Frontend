5ed81d327f7b5ee73c32ad53bbecd39c72cd44be
Eric Ebulu <ekumamait@Erics-MacBook-Pro.local>
2022-02-18 12:32:12 +0300

    add mapbox gl to display map and react fetch streams to fetch data from backend as a stream

---
+++ redacre/README.md
@@ -0,0 +1,43 @@
+##### WELCOME TO RedAcre-Locator-Frontend
+------------
+
+##### TABLE OF CONTENT;
+
+------------
+
+- [x] **DESCRIPTION**
+- [x] **PROJECT SETUP**
+- [x] **TECHNOLOGIES**
+
+------------
+
+###### :page_facing_up: DESCRIPTION;
+
+A React App to track Popeye's Location. This is an application that loads coordinates from a GeoJSON file to a mongoDB and streams them to the fronend using mongoDB cursor. The main focus of this build in its present is the implementation of the frontend of the application using React.
+
+------------
+
+##### PROJECT SETUP
+
+------------
+
+1. Clone the Repository
+` https://github.com/ekumamait/RedAcre-Frontend.git`
+
+2. Navigate to the application directory
+` cd RedAcre-Frontend/redacre `
+
+3. install all dependencies
+` npm install `
+
+4. Run the application
+` npm start `
+
+------------
+
+###### TECHNOLOGIES USED;
+
+React.js
+Mapbox
+
+------------
--- redacre/src/App.js
+++ redacre/src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
 import Map from './components/Map';
+import { Oval } from "react-loader-spinner";
+import styled from 'styled-components';
+
+const LoaderContainer = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    font-weight: 500;
+    color: #fff;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(to right top, #161c0a, #24342e, #3e4c4e, #616569, #808080);
+    top: 0;
+    left: 0;
+`;
+
+export const Title = styled.span`
+    font-size: ${props => props.fSize || '36px'};
+    margin-bottom: ${props => props.mb || '15px'};
+    font-weight: bold;
+    color: #fff;
+`
 
 function App() {
+  const [isGifLoading, SetisGifLoading] = useState(true);
+  setTimeout(() => {
+      SetisGifLoading(false)
+  }, 1000)
 
   return (
-    <div>
-      <Map />
-    </div>
+    <>
+      {
+        isGifLoading ?
+          <LoaderContainer >
+              <Oval color="#eb7924" />
+              <div> <Title>RedAcre Locator</Title> </div>
+          </LoaderContainer>
+      :
+        <>
+          <div>
+            <Map />
+          </div>
+        </>
+      }
+    </>
   );
 }
 
